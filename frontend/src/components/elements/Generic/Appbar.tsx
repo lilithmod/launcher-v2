b0587521fb91297f2878dc6551b94d1f1b22a2c6
theMackabu <theMackabu@gmail.com>
2023-10-21 16:23:28 -0700

    QOL: add animations

--- frontend/src/components/elements/Generic/Appbar.tsx
+++ frontend/src/components/elements/Generic/Appbar.tsx
@@ -8,16 +8,13 @@ import { Link, useLocation } from 'react-router-dom';
 import { XIcon, MinusIcon } from '@heroicons/react/outline';
 import { Quit, BrowserOpenURL, WindowMinimise } from '@/wailsjs/runtime';
 
+const LinkStyle = tw`text-base font-semibold text-neutral-300 -my-2 px-2 py-[5px] transition border-transparent relative after:bg-rose-500 after:absolute after:h-0.5 after:w-0 after:bottom-0 after:left-0 hover:after:w-full after:transition-all after:duration-300`;
+
 const NavLink = (props: { to: string; name: any }) => {
 	const location = useLocation();
 
 	return (
-		<Link
-			to={props.to}
-			css={[
-				tw`text-base font-semibold border-b-2 text-neutral-300 -my-2 px-2 py-1 transition border-transparent hover:border-rose-500`,
-				location.pathname.startsWith(`/${props.to.split('/')[1]}`) && tw`border-rose-500 pointer-events-none`,
-			]}>
+		<Link to={props.to} css={[LinkStyle, location.pathname.startsWith(`/${props.to.split('/')[1]}`) && tw`after:w-full pointer-events-none`]}>
 			{props.name}
 		</Link>
 	);
@@ -29,34 +26,35 @@ const Appbar = () => {
 
 	const PathBasedStyles = (pathname: String) => {
 		return pathname != '/launch'
-		? AppSettings!.blur
-			? tw`bg-neutral-700 backdrop-blur-lg backdrop-filter bg-opacity-[0.34]`
-			: AppSettings!.sidebar && tw`bg-neutral-800 shadow-xl`
-		: tw`pt-3.5`
-	}
+			? AppSettings!.blur
+				? tw`bg-neutral-700 backdrop-blur-lg backdrop-filter bg-opacity-[0.34]`
+				: AppSettings!.sidebar
+				? tw`bg-neutral-800 shadow-xl`
+				: tw`bg-neutral-800`
+			: tw`pt-3.5`;
+	};
 
 	return (
-		<div
-			tw="max-w-7xl mx-auto px-6 fixed w-full z-50 pt-2"
-			css={PathBasedStyles(location.pathname)}
-			className="draggable">
+		<div tw="max-w-7xl mx-auto px-6 fixed w-full z-50 pt-2" css={PathBasedStyles(location.pathname)} className="draggable">
 			<div tw="flex justify-between items-center py-2 space-x-6">
 				<div tw="flex justify-start lg:w-0 lg:flex-1 font-bold text-white text-xl">
 					<img src={LilithLogo} css={location.pathname == '/launch' ? tw`hidden` : tw`h-9`} alt="Lilith" />
 				</div>
 				<NavLink to="/launch" name="Home" />
 				<NavLink to="/settings/general" name="Settings" />
-				<button
-					onClick={() => BrowserOpenURL('https://lilith.rip/pricing')}
-					tw="text-base font-semibold border-b-2 text-neutral-300 -my-2 px-2 py-1 transition border-transparent hover:border-rose-500 hover:bg-opacity-50">
+				<button onClick={() => BrowserOpenURL('https://lilith.rip/pricing')} css={LinkStyle}>
 					Plans
 				</button>
 				<div tw="flex items-center justify-end md:flex-1 lg:w-0">
-					<button onClick={() => WindowMinimise()} tw="whitespace-nowrap text-base font-medium text-neutral-500 hover:text-white transition">
-						<MinusIcon tw="w-6 h-6 stroke-1" />
+					<button
+						onClick={() => WindowMinimise()}
+						tw="whitespace-nowrap text-base font-medium text-neutral-500 hover:(text-white scale-[1.06]) transition">
+						<MinusIcon tw="w-6 h-6 stroke-2" />
 					</button>
-					<button onClick={() => Quit()} tw="whitespace-nowrap text-base font-medium text-neutral-500 hover:text-white transition ml-5">
-						<XIcon tw="w-6 h-6 stroke-1" />
+					<button
+						onClick={() => Quit()}
+						tw="whitespace-nowrap text-base font-medium text-neutral-500 hover:(text-white scale-[1.06]) transition ml-5">
+						<XIcon tw="w-6 h-6 stroke-2" />
 					</button>
 				</div>
 			</div>
